2cc60246a999fc7b90040c46149663a47e80f3bd
Remy Sharp <remy@remysharp.com>
2014-09-16 14:11:36 +0100

    Removed the rewrite code
    In favour of using req.url updates

--- index.js
+++ index.js
@@ -1,92 +1,99 @@
+'use strict';
 var matcher = require('./matcher');
-var formatter = require('./formatter');
 
 var METHODS      = ['get', 'post', 'put', 'del'   , 'delete', 'head', 'options'];
 var HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'DELETE', 'HEAD', 'OPTIONS'];
 
 var noop = function() {};
 var error = function(res) {
-	return function() {
-		res.statusCode = 404;
-		res.end();
-	};
+  return function() {
+    res.statusCode = 404;
+    res.end();
+  };
 };
 var router = function() {
-	var methods = {};
-	var traps = {};
+  var methods = {};
+  var traps = {};
 
-	HTTP_METHODS.forEach(function(method) {
-		methods[method] = [];
-	});
+  HTTP_METHODS.forEach(function(method) {
+    methods[method] = [];
+  });
 
-	var route = function(req, res, next) {
-		var method = methods[req.method];
-		var trap = traps[req.method];
-		var index = req.url.indexOf('?');
-		var url = index === -1 ? req.url : req.url.substr(0, index);
-		var i = 0;
+  var route = function(req, res, next) {
+    var method = methods[req.method];
+    var trap = traps[req.method];
+    var index = req.url.indexOf('?');
+    var url = index === -1 ? req.url : req.url.substr(0, index);
+    var i = 0;
 
-		next = next || error(res);
-		if (!method) return next();
+    req.originalURL = url;
 
-		var loop = function(err) {
-			if (err) return next(err);
-			while (i < method.length) {
-				var route = method[i];
+    next = next || error(res);
+    if (!method) {
+      return next();
+    }
 
-				i++;
-				req.params = route.pattern(url);
-				if (!req.params) continue;
-				if (route.rewrite) {
-					req.url = url = route.rewrite(req.params)+(index === -1 ? '' : req.url.substr(index));
-				}
-				route.fn(req, res, loop);
-				return;
-			}
-			if (!trap) return next();
-			trap(req, res, next);
-		};
+    var loop = function(err) {
+      if (err) {
+        return next(err);
+      }
+      while (i < method.length) {
+        var route = method[i];
 
-		loop();
-	};
+        i++;
 
-	METHODS.forEach(function(method, i) {
-		route[method] = function(pattern, rewrite, fn) {
-			if (Array.isArray(pattern)) {
-				pattern.forEach(function(item) {
-					route[method](item, rewrite, fn);
-				});
-				return;
-			}
+        // allow the user to update the url from inside their middleware
+        var index = req.url.indexOf('?');
+        var url = index === -1 ? req.url : req.url.substr(0, index);
 
-			if (!fn && !rewrite)                      return route[method](null, null, pattern);
-			if (!fn && typeof rewrite === 'string')   return route[method](pattern, rewrite, route);
-			if (!fn && typeof rewrite === 'function') return route[method](pattern, null, rewrite);
-			if (!fn) return route;
+        req.params = route.pattern(url);
+        if (!req.params) {
+          continue;
+        }
 
-			(route.onmount || noop)(pattern, rewrite, fn);
+        route.fn(req, res, loop);
+        return;
+      }
+      if (!trap) {
+        return next();
+      }
+      trap(req, res, next);
+    };
 
-			if (!pattern) {
-				traps[HTTP_METHODS[i]] = fn;
-				return route;
-			}
+    loop();
+  };
 
-			methods[HTTP_METHODS[i]].push({
-				pattern:matcher(pattern),
-				rewrite:formatter(rewrite),
-				fn:fn
-			});
-			return route;
-		};
-	});
-	route.all = function(pattern, rewrite, fn) {
-		METHODS.forEach(function(method) {
-			route[method](pattern, rewrite, fn);
-		});
-		return route;
-	};
+  METHODS.forEach(function(method, i) {
+    route[method] = function(pattern, fn) {
+      if (Array.isArray(pattern)) {
+        pattern.forEach(function(item) {
+          route[method](item, fn);
+        });
+        return;
+      }
 
-	return route;
+      (route.onmount || noop)(pattern, fn);
+
+      if (typeof pattern === 'function') {
+        traps[HTTP_METHODS[i]] = pattern;
+        return route;
+      }
+
+      methods[HTTP_METHODS[i]].push({
+        pattern:matcher(pattern),
+        fn:fn
+      });
+      return route;
+    };
+  });
+  route.all = function(pattern, fn) {
+    METHODS.forEach(function(method) {
+      route[method](pattern, fn);
+    });
+    return route;
+  };
+
+  return route;
 };
 
 module.exports = router;
